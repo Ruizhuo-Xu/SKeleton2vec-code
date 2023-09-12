aef31a3cb73debf97955422e10f50d172530d1bd
ruizhuo <ruizhuoxu@163.com>
2023-09-12 10:41:04 +0800

    achieve_skeleton2vec2.0

--- models/skeleton2vec.py
+++ models/skeleton2vec.py
@@ -159,6 +159,125 @@ class Skeleton2Vec(nn.Module):
         return x, y
 
 
+@models.register('Skeleton2Vec2')
+class Skeleton2Vec2(nn.Module):
+    """
+    Data2Vec main module.
+
+    Args:
+         encoder (nn.Module): The encoder module like BEiT, ViT, etc.
+         cfg (omegaconf.DictConfig): The config containing model properties
+    """
+
+    def __init__(self, model_spec, ema_spec,
+                 average_top_k_layers,
+                 norm_target_per_layer='layer_norm',
+                 normalize_targets=True,
+                 **kwargs):
+        super(Skeleton2Vec2, self).__init__()
+        self.auto_encoder = models.make(model_spec)
+        # self.embed_dim = self.encoder.emb_size
+        # self.temporal_segment_size = self.encoder.embedding.temporal_segment_size
+
+        self.average_top_k_layers = average_top_k_layers
+        self.normalize_targets = normalize_targets
+        assert norm_target_per_layer in ['layer_norm', 'instance_norm'], \
+        f'norm_target_per_layer must be one of [layer_norm, instance_norm], got {norm_target_per_layer}'
+        self.norm_target_per_layer = norm_target_per_layer
+        self.__dict__.update(kwargs)
+
+        # param init
+        self.apply(self._init_weights)
+        # self.fix_init_weight()
+
+        self.ema = models.make(ema_spec, args={'model': self.auto_encoder})  # EMA acts as the teacher
+        self.ema_decay = self.ema.decay
+        self.ema_end_decay = self.ema.ema_end_decay
+        self.ema_anneal_end_step = self.ema.ema_anneal_end_step
+    
+    # def fix_init_weight(self):
+    #     def rescale(param, layer_id):
+    #         param.div_(math.sqrt(2.0 * layer_id))
+
+    #     for layer_id, layer in enumerate(self.encoder.encoder.layers):
+    #         rescale(layer.attn.proj.weight.data, layer_id + 1)
+    #         rescale(layer.mlp.fc2.weight.data, layer_id + 1)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+        elif isinstance(m, nn.Conv2d):
+            trunc_normal_(m.weight, std=.02)
+            if m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+
+    def ema_step(self):
+        """
+        One EMA step for the offline model until the ending decay value is reached
+        """
+        if self.ema_decay != self.ema_end_decay:
+            if self.ema.num_updates >= self.ema_anneal_end_step:
+                decay = self.ema_end_decay
+            else:
+                decay = self.ema.get_annealed_rate(
+                    self.ema_decay,
+                    self.ema_end_decay,
+                    self.ema.num_updates,
+                    self.ema_anneal_end_step,
+                )
+            self.ema.decay = decay
+        if self.ema.decay < 1:
+            self.ema(self.auto_encoder)
+
+    def forward(self, src: torch.Tensor, mask_ratio=0., num_masked_views=1, **kwargs):
+        """
+        Data2Vec forward method.
+
+        Args:
+            src: src tokens (masked inputs for training)
+            trg: trg tokens (unmasked inputs for training but left as `None` otherwise)
+            mask: bool masked indices, Note: if a modality requires the inputs to be masked before forward this param
+            has no effect. (see the Encoder for each modality to see if it uses mask or not)
+
+        Returns:
+            Either encoder outputs or a tuple of encoder + EMA outputs
+
+        """
+        # Multi-mask Training (data2vec2.0)
+        src_repeat = src.repeat_interleave(num_masked_views, dim=0)
+        x, mask = self.auto_encoder(src_repeat, mask_ratio=mask_ratio)  # fetch the last layer outputs
+
+        # model forward in offline mode (teacher)
+        with torch.no_grad():
+            self.ema.model.eval()
+            y = self.ema.model.forward_encoder(src, mask_ratio=0., output_hidden_states=True)['hidden_states']  # fetch the last transformer layers outputs
+            y = y[-self.average_top_k_layers:]  # take the last k transformer layers
+
+            if self.norm_target_per_layer == 'layer_norm':
+                y = [F.layer_norm(tl.float(), tl.shape[-1:]) for tl in y]
+                y = sum(y) / len(y)
+            elif self.norm_target_per_layer == 'instance_norm':
+                y = [rearrange(tl, 'b n c -> b c n') for tl in y]
+                y = [F.instance_norm(tl.float()) for tl in y]
+                y = [rearrange(tl, 'b c n -> b n c') for tl in y]
+                y = sum(y) / len(y)
+            else:
+                raise NotImplementedError
+            if self.normalize_targets:
+                y = F.layer_norm(y.float(), y.shape[-1:])
+
+        y = y.repeat_interleave(num_masked_views, dim=0)
+        mask = mask.flatten().bool()
+        x = rearrange(x, 'b n c -> (b n) c')[mask]
+        y = rearrange(y, 'b n c -> (b n) c')[mask]
+        return x, y
+
+
 if __name__ == '__main__':
     encoder_spec = {
         'name': 'SkT',
